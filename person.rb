0a2376163dfece0d7247e0e477efb4c4f5c55f5f
unknown <okorojiebube2@gmail.com>
2022-05-17 13:06:01 -0600

    Add setter method

--- person.rb
+++ person.rb
@@ -1,9 +1,15 @@
 class Person
-  def initialize( name = "Unknown", age, parent_permission = true)
-    @id
+  def initialize( id = Math.random(), name = "Unknown" age, parent_permission = true)
+    @id = id
     @name = name
     @age = age
   end
+  
+  def get_person
+     @id
+    @name
+    @age
+  end
 
-
+  
 end
