c5df47b6b7ac3e105cc1f8337f96840dda2a8e2e
Okoroji Victor Ebube <okorojiebube2@gmail.com>
2022-05-20 23:10:33 +0100

    Update student.rb

--- student.rb
+++ student.rb
@@ -6,7 +6,11 @@ class Student < person
     super
     @classroom = classroom
   end
-
+  
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
   def play_hooky
     "¯\(ツ)/¯"
   end
