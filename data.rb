178129231a401d96ace13dc38290c1220344e0a8
unknown <okorojiebube2@gmail.com>
2022-05-25 04:09:56 -0600

    Read books from file

--- data.rb
+++ data.rb
@@ -5,3 +5,11 @@ def initialize_files
   File.write('./datas/people.json', []) unless File.exist?('./datas/people.json')
   File.write('./datas/rental.json', []) unless File.exist?('./datas/rental.json')
 end
+
+def read_books
+  books = []
+  JSON.parse(File.read('./datas/book.json')).each do |book|
+    books.push(Book.new(book['title'], book['author']))
+  end
+  books
+end
